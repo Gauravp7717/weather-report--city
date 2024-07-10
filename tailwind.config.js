ad78c58c8e817e5c470753f81d20ec890a633bdb
Gauravp7717 <114347888+Gauravp7717@users.noreply.github.com>
2024-07-10 11:52:35 +0530

    tailwind used

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,43 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth:{
+        '470': '470px',
+      },
+      fontSize: {
+        '18': '18px',
+      },
+      margin: {
+        '16': '16px',
+      },
+      height:{
+        '60': '60px',
+      },
+      padding: {
+        '10': '10px',
+        '25': '25px',
+      },
+      colors: {
+        cbg: '#ebfffc',
+      },
+      borderRadius: {
+        '20': '20px',
+        '30': '30px',
+        '50': '50px',
+      },
+      spacing: {
+        '100': '100px',
+      },
+      width: {
+        '9/10': '90%',
+        '60': '60px',
+        '16': '16px',
+      },
+      backgroundImage: {
+        'custom-gradient': 'linear-gradient(135deg, #00feba, #5b584a)',
+      },
+    },
   },
   plugins: [],
-}
-
+};
